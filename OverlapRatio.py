56e6063b3d4f7a61dd6dc075739318bb19f7a3ba
wwyqianqian <1208331933@qq.com>
2019-12-29 01:13:28 +0800

    [feat]: 增加一个标题多篇匹配功能

--- OverlapRatio.py
+++ OverlapRatio.py
@@ -5,55 +5,89 @@ import re
 
 
 def word2list(mydir, mylist):  # Read the txt file, and put every word into my list.
-    with open(mydir,'r',encoding='utf-8') as f:
-        lines = f.readlines()
+    try:
+        with open(mydir,'r',encoding='utf-8') as f:
+            lines = f.readlines()
+            for i in range(0,lines.__len__(),1):
+                for word in lines[i].split():
+                    word=word.strip(string.whitespace)
+                    mylist.append(word)
+            return mylist
 
-        for i in range(0,lines.__len__(),1):
-            for word in lines[i].split():
-                word=word.strip(string.whitespace)
-                mylist.append(word)
-        return mylist
+    except (IOError, TypeError):
+        pass
 
 
-def wordCounter(t, p):
-    for i in list1:
-        if i in list2:
-            # print(i)
-            t = int(t) + 1
 
-    # print("===============")
+def wordCounter(t, p, list1, list2):
 
-    for j in list2:
-        if j in list1:
-            # print(j)
-            p = int(p) + 1
-
-    return t, p
-
+    try:
+        for i in list1:
+            if i in list2:
+                t = int(t) + 1
+        for j in list2:
+            if j in list1:
+                p = int(p) + 1
+        return t, p
+    except (IOError, TypeError):
+        pass
 
 def getOverlapRatio(root_title_dir, root_artic_dir, list1, list2):
-    for root, dirs, files in os.walk(root_title_dir):
-        for i in files:
 
-            title_dir = root+i
-            pattern = re.compile(r'\d+')
-            num = pattern.findall(i)
-            list1 = word2list(title_dir, list1)  # t
+    temp_title_dir_list = []
+    try:
+        for root, dirs, files in os.walk(root_title_dir):
+            for i in files:
+                title_dir = root+i
+                temp_title_dir_list.append(title_dir)
 
-            artic_dir = root_artic_dir+"artic_"+num[0]+".txt"
-            list2 = word2list(artic_dir, list2)
+        print(len(temp_title_dir_list)) # 10986 个
 
-            count_t = 0 # How many t shown in p
-            count_p = 0 # How many p shown in t
-            count_t, count_p = wordCounter(count_t, count_p)
-
-            # t 与 p 的重合度 = [count(t) + count(p)] / [length(t) + length(p)]
-            OverlapRatio = float(count_t+count_p) / (len(list1)+len(list2))
-            print("文章" + num[0] + "的重合度为：" + str(OverlapRatio))
+    except (IOError, TypeError):
+        pass
 
 
+    try:
+        # for root, dirs, files in os.walk(root_title_dir):
+        #     for i in files:
+        #         title_dir = root+i
+        #         pattern1 = re.compile(r'\d+')
+        #         num1 = pattern1.findall(i)
+        #         list1 = word2list(title_dir, list1)  # t
+
+        #         print(title_dir)
+
+        for i in range(100):
+            print(temp_title_dir_list[i])
+            list1 = word2list(temp_title_dir_list[i], list1)
+            print(list1)
+
+            for root, dirs, files in os.walk(root_artic_dir):
+                for j in files:
+                    artic_dir = root+j
+                    pattern2 = re.compile(r'\d+')
+                    num2 = pattern2.findall(j)
+                    list2 = word2list(artic_dir, list2)
+
+
+                    count_t = 0 # How many t shown in p
+                    count_p = 0 # How many p shown in t
+                    count_t, count_p = wordCounter(count_t, count_p, list1, list2)
+
+                    print("count_t: " + str(count_t))
+                    print("count_p: " + str(count_p))
+
+                    # t 与 p 的重合度 = [count(t) + count(p)] / [length(t) + length(p)]
+                    OverlapRatio = float(count_t+count_p) / (len(list1)+len(list2))
+                    print("标题" + temp_title_dir_list[i] + "对应文章" + num2[0] + "的重合度为：" + str(OverlapRatio))
+
+
+                    list2 = []
             list1 = []
-            list2 = []
+
+    except (IOError, TypeError):
+        pass
+
 
 if __name__ == "__main__":
     list1 = []
@@ -63,19 +97,32 @@ if __name__ == "__main__":
 
     getOverlapRatio(root_title_dir, root_artic_dir, list1, list2)
 
-# OUTPUT:
-# 文章11495的重合度为：0.05625
-# 文章12822的重合度为：0.14240506329113925
-# 文章5977的重合度为：0.0677710843373494
-# 文章15195的重合度为：0.07588532883642496
-# 文章16488的重合度为：0.04838709677419355
-# 文章9181的重合度为：0.058365758754863814
-# 文章17796的重合度为：0.0273224043715847
-# 文章5963的重合度为：0.08333333333333333
-# 文章12188的重合度为：0.08211678832116788
-# 文章13296的重合度为：0.05039193729003359
-# 文章7812的重合度为：0.06484149855907781
-# 文章11481的重合度为：0.061391541609822645
-# 文章10947的重合度为：0.052878965922444184
-# 文章3484的重合度为：0.09297520661157024
 
+#OUTPUT:
+# count_t: 1
+# count_p: 1
+# 标题/Users/qianqian/Desktop/articles/title/title_15195.txt对应文章18645的重合度为：0.0038022813688212928
+# count_t: 0
+# count_p: 0
+# 标题/Users/qianqian/Desktop/articles/title/title_15195.txt对应文章17576的重合度为：0.0
+# count_t: 0
+# count_p: 0
+# 标题/Users/qianqian/Desktop/articles/title/title_15195.txt对应文章16668的重合度为：0.0
+# count_t: 0
+# count_p: 0
+# 标题/Users/qianqian/Desktop/articles/title/title_15195.txt对应文章1587的重合度为：0.0
+# count_t: 0
+# count_p: 0
+# 标题/Users/qianqian/Desktop/articles/title/title_15195.txt对应文章948的重合度为：0.0
+# count_t: 0
+# count_p: 0
+# 标题/Users/qianqian/Desktop/articles/title/title_15195.txt对应文章18123的重合度为：0.0
+# count_t: 0
+# count_p: 0
+# 标题/Users/qianqian/Desktop/articles/title/title_15195.txt对应文章790的重合度为：0.0
+# count_t: 0
+# count_p: 0
+# 标题/Users/qianqian/Desktop/articles/title/title_15195.txt对应文章13076的重合度为：0.0
+# count_t: 1
+# count_p: 2
+# 标题/Users/qianqian/Desktop/articles/title/title_15195.txt对应文章24的重合度为：0.003592814371257485
